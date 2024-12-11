b46175cbafdb7223a1ed528052083792bc23dad4
FelixTengBean <66650348+FelixTengBean@users.noreply.github.com>
2024-12-10 23:23:22 -0500

    minor changes

--- api/agent/config.go
+++ api/agent/config.go
@@ -97,15 +97,15 @@ func NewAgentsDB(dbPlugin *ast.ObjectItem) (agentdb.AgentDB, error) {
 // NewCRDManager returns ...
 func NewCRDManager(crdPlugin *ast.ObjectItem) (spirecrd.CRDManager, error) {
 	_, data, _ := getPluginConfig(crdPlugin)
-	
+
 	// check if data is defined
 	if data == nil {
-		return "", errors.New("SPIRECRDManager plugin ('config > plugins > SPIRECRDManager > plugin_data') not populated")
+		return nil, errors.New("SPIRECRDManager plugin ('config > plugins > SPIRECRDManager > plugin_data') not populated")
 	}
 	// decode config to struct
 	var config pluginControllerManager
 	if err := hcl.DecodeObject(&config, data); err != nil {
-		return "", errors.Errorf("Couldn't parse SPIREControllerManager config: %v", err)
+		return nil, errors.Errorf("Couldn't parse SPIREControllerManager config: %v", err)
 	}
 
 	fmt.Println("CRD Controller configured. WARNING: This is currently a no-op")
