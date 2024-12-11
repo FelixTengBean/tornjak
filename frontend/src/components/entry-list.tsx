4c3500f86cab04d14a26a237f5493304e7b56476
FelixTengBean <66650348+FelixTengBean@users.noreply.github.com>
2024-12-10 23:34:25 -0500

    The data loading logic is now encapsulated in loadEntries(), lifecycle methods are simpler

--- frontend/src/components/entry-list.tsx
+++ frontend/src/components/entry-list.tsx
@@ -2,99 +2,129 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import IsManager from './is_manager';
 import Table from "tables/entries-list-table";
-import TornjakApi from "./tornjak-api-helpers"
+import TornjakApi from "./tornjak-api-helpers";
 import {
   serverSelectedFunc,
   entriesListUpdateFunc,
   tornjakMessageFunc,
 } from 'redux/actions';
-// import PropTypes from "prop-types"; // needed for testing will be removed on last pr
 import { RootState } from 'redux/reducers';
 import {
-  EntriesList, 
+  EntriesList,
   TornjakServerInfo,
   DebugServerInfo,
-} from './types'
+} from './types';
 
 type EntryListProp = {
-  // tornjak server debug info of the selected server
   globalDebugServerInfo: DebugServerInfo,
-  // dispatches a payload for the server selected and has a return type of void
   serverSelectedFunc: (globalServerSelected: string) => void,
-  // dispatches a payload for list of entries with their metadata info as an array of EntriesListType and has a return type of void
   entriesListUpdateFunc: (globalEntriesList: EntriesList[]) => void,
-  // dispatches a payload for the tornjak error messsege and has a return type of void
   tornjakMessageFunc: (globalErrorMessage: string) => void,
-  // the selected server for manager mode 
   globalServerSelected: string,
-  // tornjak error messege
   globalErrorMessage: string,
-  // list of available entries as array of EntriesListType
   globalEntriesList: EntriesList[],
-  // tornjak server info of the selected server
   globalTornjakServerInfo: TornjakServerInfo,
-}
+};
 
-type EntryListState = {}
+type EntryListState = {};
 
-const Entry = (props: { entry: EntriesList }) => (
-  <tr>
-    <td>{props.entry.id}</td>
-    <td>{"spiffe://" + props.entry.spiffe_id.trust_domain + props.entry.spiffe_id.path}</td>
-    <td>{"spiffe://" + props.entry.parent_id.trust_domain + props.entry.parent_id.path}</td>
-    <td>{props.entry.selectors.map(s => s.type + ":" + s.value).join(', ')}</td>
-    <td><div style={{ overflowX: 'auto', width: "400px" }}>
-      <pre>{JSON.stringify(props.entry, null, ' ')}</pre>
-    </div></td>
-  </tr>
-)
+/**
+ * Renders a single entry row in the entries table.
+ * @param props.entry - The entry object containing SPIFFE IDs and associated data.
+ */
+const Entry = (props: { entry: EntriesList }) => {
+  const { entry } = props;
+  const spiffeId = "spiffe://" + entry.spiffe_id.trust_domain + entry.spiffe_id.path;
+  const parentId = "spiffe://" + entry.parent_id.trust_domain + entry.parent_id.path;
+  const selectors = entry.selectors.map(s => s.type + ":" + s.value).join(', ');
 
+  return (
+    <tr>
+      <td>{entry.id}</td>
+      <td>{spiffeId}</td>
+      <td>{parentId}</td>
+      <td>{selectors}</td>
+      <td>
+        <div style={{ overflowX: 'auto', width: "400px" }}>
+          <pre>{JSON.stringify(entry, null, ' ')}</pre>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+/**
+ * EntryList component displays a list of SPIRE entries in a table.
+ * It fetches entries from the server (or locally) depending on the environment and updates the Redux store.
+ */
 class EntryList extends Component<EntryListProp, EntryListState> {
   TornjakApi: TornjakApi;
+
   constructor(props: EntryListProp) {
     super(props);
     this.TornjakApi = new TornjakApi(props);
-    this.state = {}
+    this.state = {};
+    this.loadEntries = this.loadEntries.bind(this);
   }
 
+  /**
+   * On mount, load the initial entries depending on manager mode and selected server.
+   */
   componentDidMount() {
-    if (IsManager) {
-      if (this.props.globalServerSelected !== "") {
-        this.TornjakApi.populateEntriesUpdate(this.props.globalServerSelected, this.props.entriesListUpdateFunc, this.props.tornjakMessageFunc)
-      }
-    } else {
-      this.TornjakApi.populateLocalEntriesUpdate(this.props.entriesListUpdateFunc, this.props.tornjakMessageFunc)
-    }
+    this.loadEntries();
   }
 
+  /**
+   * On update, if the selected server changes in manager mode or if debug info changes in local mode, 
+   * re-fetch entries.
+   */
   componentDidUpdate(prevProps: EntryListProp) {
-    if (IsManager) {
-      if (prevProps.globalServerSelected !== this.props.globalServerSelected) {
-        this.TornjakApi.populateEntriesUpdate(this.props.globalServerSelected, this.props.entriesListUpdateFunc, this.props.tornjakMessageFunc)
-      }
-    } else {
-      if (prevProps.globalDebugServerInfo !== this.props.globalDebugServerInfo) {
-        this.TornjakApi.populateLocalEntriesUpdate(this.props.entriesListUpdateFunc, this.props.tornjakMessageFunc)
-      }
+    const serverChanged = prevProps.globalServerSelected !== this.props.globalServerSelected;
+    const debugInfoChanged = prevProps.globalDebugServerInfo !== this.props.globalDebugServerInfo;
+
+    if (IsManager && serverChanged) {
+      this.loadEntries();
+    } else if (!IsManager && debugInfoChanged) {
+      this.loadEntries();
     }
   }
 
-  entryList() {
-    if (this.props.globalEntriesList === undefined) return ""
-    return this.props.globalEntriesList.map((currentEntry: EntriesList) => {
-      return <Entry key={currentEntry.id} entry={currentEntry} />
-    })
+  /**
+   * Load entries from the Tornjak server or locally, depending on the current mode.
+   * - In manager mode, requires a selected server.
+   * - Otherwise, loads local entries.
+   */
+  private loadEntries(): void {
+    const { globalServerSelected, entriesListUpdateFunc, tornjakMessageFunc } = this.props;
+    if (IsManager) {
+      if (globalServerSelected !== "") {
+        this.TornjakApi.populateEntriesUpdate(globalServerSelected, entriesListUpdateFunc, tornjakMessageFunc);
+      }
+    } else {
+      this.TornjakApi.populateLocalEntriesUpdate(entriesListUpdateFunc, tornjakMessageFunc);
+    }
+  }
+
+  /**
+   * Generates a list of Entry components for rendering in the table.
+   */
+  private entryList() {
+    const { globalEntriesList } = this.props;
+    if (!globalEntriesList || globalEntriesList.length === 0) return "";
+    return globalEntriesList.map((currentEntry: EntriesList) => {
+      return <Entry key={currentEntry.id} entry={currentEntry} />;
+    });
   }
 
   render() {
+    const { globalErrorMessage } = this.props;
+
     return (
       <div data-test="entry-list">
         <h3>Entries List</h3>
-        {this.props.globalErrorMessage !== "OK" &&
+        {globalErrorMessage !== "OK" && 
           <div className="alert-primary" role="alert">
-            <pre>
-              {this.props.globalErrorMessage}
-            </pre>
+            <pre>{globalErrorMessage}</pre>
           </div>
         }
         <br /><br />
@@ -102,10 +132,9 @@ class EntryList extends Component<EntryListProp, EntryListState> {
           <Table data={this.entryList()} id="table-1" />
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 const mapStateToProps = (state: RootState) => ({
   globalServerSelected: state.servers.globalServerSelected,
@@ -113,21 +142,11 @@ const mapStateToProps = (state: RootState) => ({
   globalErrorMessage: state.tornjak.globalErrorMessage,
   globalTornjakServerInfo: state.servers.globalTornjakServerInfo,
   globalDebugServerInfo: state.servers.globalDebugServerInfo,
-})
-
-// Note: Needed for UI testing - will be removed after
-// EntryList.propTypes = {
-//   globalServerSelected: PropTypes.string,
-//   globalEntriesList: PropTypes.array,
-//   globalErrorMessage: PropTypes.string,
-//   serverSelectedFunc: PropTypes.func,
-//   entriesListUpdateFunc: PropTypes.func,
-//   tornjakMessageFunc: PropTypes.func,
-// };
+});
 
 export default connect(
   mapStateToProps,
   { serverSelectedFunc, entriesListUpdateFunc, tornjakMessageFunc }
-)(EntryList)
+)(EntryList);
 
 export { EntryList };
